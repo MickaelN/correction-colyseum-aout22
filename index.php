cb2c4dca4493ff123f34e049d3fbcb2e85d406ff
Mickael Noel <mickael.noel@novei.fr>
2022-08-19 14:58:38 +0200

    Correction de l'exercice 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="ex01.php">Exercice 1</a></li>
+        <li><a href="ex02.php">Exercice 2</a></li>
+        <li><a href="ex03.php">Exercice 3</a></li>
+        <li><a href="ex04.php">Exercice 4</a></li>
+        <li><a href="ex05.php">Exercice 5</a></li>
+    </ul>
+</body>
+</html>
